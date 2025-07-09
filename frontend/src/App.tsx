5db98abea68f23456ef13abd7212447cd45510f4
benjamin-macmichael <benjaminmac21@gmail.com>
2025-07-08 23:15:25 -0600

    Initialize frontend with Vite React + TS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>CollabDocEditor</h1>
+      <p>Welcome to the collaborative document editor frontend</p>
+    </>
+  )
+}
+
+export default App
